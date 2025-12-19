f4da08ab4a9ebd121625789797387f59a8b76247
Adam Grzeszczuk <103129308+adamu86@users.noreply.github.com>
2025-12-19 20:59:34 +0100

    pmx naming

--- crossover/pmx.py
+++ crossover/pmx.py
@@ -4,20 +4,20 @@ import random
 def pmx(parent1, parent2):
     size = len(parent1)
 
+    child = [None] * size
+
     start, end = sorted(random.sample(range(size), 2))
 
-    offspring = [0] * size
-
-    offspring[start:end] = parent1[start:end]
+    child[start:end] = parent1[start:end]
 
     segment = parent1[start:end]
 
     for i in (*range(0, start), *range(end, size)):
-        candidate = parent2[i]
+        gene = parent2[i]
 
-        while candidate in segment:
-            candidate = parent2[parent1.index(candidate)]
+        while gene in segment:
+            gene = parent2[parent1.index(gene)]
 
-        offspring[i] = candidate
+        child[i] = gene
 
-    return offspring
+    return child
